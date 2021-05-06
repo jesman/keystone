5260b46c3a72664748cd784883cf2d6de662cff2
Tim Leslie <timl@thinkmill.com.au>
2021-05-06 13:55:55 +1000

    WIP

--- examples/with-auth/schema.ts
+++ examples/with-auth/schema.ts
@@ -23,7 +23,7 @@ export const lists = createSchema({
     fields: {
       name: text({ isRequired: true }),
       // Added an email and password pair to be used with authentication
-      // The email address is going to be used as the identify field, so it's
+      // The email address is going to be used as the identity field, so it's
       // important that we set both isRequired and isUnique
       email: text({ isRequired: true, isUnique: true }),
       // The password field stores a hash of the supplied password, and
